566ca1b7b076e3752918824d14cdf8909ceab60d
Leonardo Avendaño <leonardo.avendano@phinxlab.com>
2021-11-18 22:55:04 -0300

    Add scalable solution

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello!');
+
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
+
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const sorter = new Sorter(numbersCollection);
+sorter.sort();
+console.log(numbersCollection);
